866a6094a3283e0735f00875f10a5d1bfc50334b
EGem Media YouTube <milotaurus369@gmail.com>
2026-06-03 15:40:04 -0500

    Update eBlock Adblocker-1.0.6.js

--- eBlock Adblocker-1.0.6.js
+++ eBlock Adblocker-1.0.6.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @name         eBlock Adblocker
+// @license      MIT
 // @namespace    https://tampermonkey.net/
 // @version      1.0.6
 // @description  eBlock (or EADB) blocks all ads at no cost! Easily get rid of ads without any* problems.
